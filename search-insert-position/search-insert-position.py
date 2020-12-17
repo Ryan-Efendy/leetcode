c147693c280ebfafa270db112b253ba3b586ae6c
Ryan Efendy <ryanefendy95@gmail.com>
2020-12-17 15:04:26 -0800

    Time: 52 ms (32.25%), Space: 15 MB (24.56%) - LeetHub

--- search-insert-position/search-insert-position.py
+++ search-insert-position/search-insert-position.py
@@ -1,10 +1,20 @@
 class Solution:
+    '''
+     0,1,2,3 4
+    [1,3,5,6] target=2
+       l           
+     m r    
+     0,1,2,3 4
+    [1,3,5,6] target=7
+           l
+           m r
+    '''
     def searchInsert(self, nums: List[int], target: int) -> int:
         l, r = 0, len(nums)
         while l < r:
             m = l + (r-l)//2
-            if nums[m] >= target:
-                r = m
-            else:
+            if nums[m] < target:
                 l = m + 1
+            else:
+                r = m
         return l
