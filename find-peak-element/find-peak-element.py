580b84c59ba96d221395d6281b1c8f18dc1090b8
Ryan Efendy <ryanefendy95@gmail.com>
2020-12-17 13:28:38 -0800

    Time: 44 ms (68.27%), Space: 14.3 MB (48.15%) - LeetHub

--- find-peak-element/find-peak-element.py
+++ find-peak-element/find-peak-element.py
@@ -1,32 +1,10 @@
 class Solution:
-    '''
-            0,1,2,3,4,5,6,7,8,9,10
-    nums = [1,2,3,4,5,6,7,8,1,2,3]
-            l         m
-                                r
-    
-            0,1,2,3,4,5,6
-    nums = [1,2,1,3,5,6,4]
-            l 
-                  m   r    
-                  
-            0,1,2,3,4,5,6,7      
-    nums = [1,2,3,4,5,6,7,8]
-            l     m
-                          r
-    '''
     def findPeakElement(self, nums: List[int]) -> int:
-        def condition(value):
-            return nums[value] > nums[value+1]              
-        
-        if len(nums) == 1: return 0
-​
         l, r = 0, len(nums)-1
-        
         while l < r:
             m = l + (r-l)//2
-            if condition(m):
-                r = m
-            else:
+            if nums[m] < nums[m+1]:
                 l = m + 1
+            else:
+                r = m
         return l
