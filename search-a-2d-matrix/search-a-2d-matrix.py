3ad02eb418208a8cd5d93165371d65a2d21c93cd
Ryan Efendy <ryanefendy95@gmail.com>
2020-12-18 10:30:30 -0800

    Time: 48 ms (14.25%), Space: 14.9 MB (15.91%) - LeetHub

--- search-a-2d-matrix/search-a-2d-matrix.py
+++ search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,25 +1,13 @@
 class Solution:
-    '''
-    [ i
-     [1, 3, 5, 7 ],
-     [10,11,16,20],
-     [23,30,34,50]
-               j
-    ]
-    
-    '''
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool: 
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         if not len(matrix) or not len(matrix[0]): return False
-        m = len(matrix) # row
-        n = len(matrix[0]) # col
-        l, r = 0, m*n-1
-        while l < r:
-            mid = l + (r-l)//2
-            row = mid // n
-            col = mid % n
-            if matrix[row][col] < target:
-                l = mid + 1
-            else:
-                r = mid
-        return True if matrix[l//n][l%n] == target else False
         
+        i, j = 0, len(matrix[0])-1
+        while i <= len(matrix)-1 and j >= 0:
+            if matrix[i][j] == target:
+                return True
+            elif matrix[i][j] < target:
+                i += 1
+            else:
+                j -= 1
+        return False
