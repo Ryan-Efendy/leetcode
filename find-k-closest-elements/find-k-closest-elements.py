2802db2b637d8a03c563a6c16dc533a9ab8fc2a3
Ryan Efendy <ryanefendy95@gmail.com>
2020-12-17 16:31:43 -0800

    Time: 288 ms (72.30%), Space: 15.7 MB (31.12%) - LeetHub

--- find-k-closest-elements/find-k-closest-elements.py
+++ find-k-closest-elements/find-k-closest-elements.py
@@ -2,9 +2,10 @@ class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
         l, r = 0, len(arr)-k
         while l < r:
-            m = l + (r-l)//2
+            m = l + (r-l)//2  # m is the start of window size k
             if x - arr[m] > arr[m+k] - x:
                 l = m + 1
             else:
                 r = m
         return arr[l:l+k]
+            
