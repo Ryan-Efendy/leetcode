7eeba0cdc2bad77b3641ab8e791ead4c63ad865d
Ryan Efendy <ryanefendy95@gmail.com>
2020-12-23 14:40:43 -0800

    Time: 56 ms (27.33%), Space: 14.3 MB (93.56%) - LeetHub

--- valid-anagram/valid-anagram.py
+++ valid-anagram/valid-anagram.py
@@ -1,12 +1,8 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-        counter = collections.Counter(s)
-        for ch in t:
-            counter[ch] -= 1
-        
-        for ch in s:
-            if counter[ch]: return False
-        
-        return True
+        if len(s) != len(t): return False
+        freq = [0]*26
+        for ch1, ch2 in zip(s, t):
+            freq[ord(ch1) - ord('a')] += 1
+            freq[ord(ch2) - ord('a')] -= 1
+        return not any(freq)
