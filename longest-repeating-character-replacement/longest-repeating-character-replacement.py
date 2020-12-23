86f118b8bb7dc727c35df201d304c99087c92cd9
Ryan Efendy <ryanefendy95@gmail.com>
2020-12-23 12:33:20 -0800

    Time: 108 ms (77.44%), Space: 14.4 MB (35.98%) - LeetHub

--- longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,22 +1,13 @@
 class Solution:
-    '''
-    s = "AABABBBBBBBA", k = 1
-         l
-             r
-    {
-        A: 3
-        B: 1
-    }
-    '''
     def characterReplacement(self, s: str, k: int) -> int:
-        i, j, max_len, max_count = 0, 0, 0, 0
         d = collections.defaultdict(int)
+        i, j, maxCount,maxLen = 0, 0, 0, 0
         while j < len(s):
             d[s[j]] += 1
-            max_count = max(max_count, d[s[j]])
+            maxCount = max(maxCount, d[s[j]])
             j += 1
-            while j-i-max_count > k:
+            while j - i - maxCount > k:
                 d[s[i]] -= 1
                 i += 1
-            max_len = max(max_len, j - i)
-        return max_len
+            maxLen = max(maxLen, j - i)
+        return maxLen
