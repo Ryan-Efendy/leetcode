cf9e45c78af80e0ddf08728c6cf68a04a44a695a
Ryan Efendy <ryanefendy95@gmail.com>
2020-12-27 11:22:54 -0800

    Time: 460 ms (55.84%), Space: 14.3 MB (96.04%) - LeetHub

--- substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.py
+++ substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.py
@@ -8,20 +8,21 @@ class Solution:
     barfoofoobarthefoobarman
     '''
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
-        wordBag = Counter(words)   # count the freq of each word
-        wordLen, numWords = len(words[0]), len(words)
+        counter = Counter(words)   # count the freq of each word
+        n, m = len(words[0]), len(words)
         res = []
         i = 0
-        while i <= len(s) - wordLen * numWords:
+        while i <= len(s) - n * m:
             seen = defaultdict(int)   # reset for each i
-            for j in range(i, i+wordLen*numWords, wordLen):
-                currWord = s[j:j+wordLen]
-                if currWord in wordBag:
-                    seen[currWord] += 1
-                    if seen[currWord] > wordBag[currWord]: break
-                else:
+            j = 0
+            while j < m:
+                currWord = s[i+j*n:i+(j+1)*n]
+                if currWord not in counter:
                     break
-            if seen == wordBag:
+                seen[currWord] += 1
+                if seen[currWord] > counter[currWord]: break
+                j += 1
+            if j == m:
                 res.append(i)
             i += 1
         return res
