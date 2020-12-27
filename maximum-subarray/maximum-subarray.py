123814cf45f7ae6a97feee4e85eb2f342fe87c18
Ryan Efendy <ryanefendy95@gmail.com>
2020-12-27 13:26:37 -0800

    Time: 68 ms (45.02%), Space: 14.9 MB (45.02%) - LeetHub

--- maximum-subarray/maximum-subarray.py
+++ maximum-subarray/maximum-subarray.py
@@ -1,8 +1,8 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        local_max = nums[0]
-        global_max = local_max
+        local_max, global_max = nums[0], nums[0]
         for num in nums[1:]:
             local_max = max(local_max+num, num)
             global_max = max(global_max, local_max)
         return global_max
+            
