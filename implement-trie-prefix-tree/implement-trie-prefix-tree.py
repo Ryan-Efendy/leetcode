be1b69254b72b8e785a3168a4f5b652e39afb464
Ryan Efendy <ryanefendy95@gmail.com>
2021-02-18 09:15:42 -0800

    Time: 172 ms (73.48%), Space: 31.4 MB (69.68%) - LeetHub

--- implement-trie-prefix-tree/implement-trie-prefix-tree.py
+++ implement-trie-prefix-tree/implement-trie-prefix-tree.py
@@ -1,52 +1,41 @@
 class TrieNode:
-    def __init__(self, val: str = ''):
-        self.val = val
+    def __init__(self):
         self.children = {}
         self.isEnd = False
 
 class Trie:
-
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
+        """Initialize your data structure here."""
         self.root = TrieNode()
 
     def insert(self, word: str) -> None:
-        """
-        Inserts a word into the trie.
-        """
+        """Inserts a word into the trie."""
+        if not word or not len(word): return
         curr = self.root
         for ch in word:
             if ch not in curr.children:
-                curr.children[ch] = TrieNode(ch)
+                curr.children[ch] = TrieNode()
             curr = curr.children[ch]
         curr.isEnd = True
-                
-    def startsWith(self, prefix: str) -> bool:
-        """
-        Returns if there is any word in the trie that starts with the given prefix.
-        """
-        curr = self.root
-        for ch in prefix:
-            if ch in curr.children:
-                curr = curr.children[ch]
-            else:
-                return False
-        return True
 
     def search(self, word: str) -> bool:
-        """
-        Returns if the word is in the trie.
-        """
+        """Returns if the word is in the trie."""
+        if not word or not len(word): return False
         curr = self.root
         for ch in word:
-            if ch in curr.children:
-                curr = curr.children[ch]
-            else:
-                return False
+            if ch not in curr.children: return False
+            curr = curr.children[ch]
         return curr.isEnd
         
+    def startsWith(self, prefix: str) -> bool:
+        """Returns if there is any word in the trie that starts with the given prefix."""
+        if not prefix or not len(prefix): return False
+        curr = self.root
+        for ch in prefix:
+            if ch not in curr.children: return False
+            curr = curr.children[ch]
+        return True
+
 # Your Trie object will be instantiated and called as such:
 # obj = Trie()
 # obj.insert(word)
