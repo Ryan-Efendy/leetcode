787fcf37b7f1f85be2c2cb0b587038b37a15fec9
Ryan Efendy <ryanefendy95@gmail.com>
2021-02-22 14:32:09 -0800

    Time: 688 ms (64.95%), Space: 17.4 MB (41.26%) - LeetHub

--- 01-matrix/01-matrix.py
+++ 01-matrix/01-matrix.py
@@ -4,6 +4,8 @@ class Solution:
         m, n = len(matrix), len(matrix[0])
         q = collections.deque()
         visitted = set()
+        
+        # init
         for i in range(m):
             for j in range(n):
                 if matrix[i][j] == 0:
@@ -11,17 +13,16 @@ class Solution:
                     visitted.add((i, j))
         
         directions = [(0,1), (1,0), (0,-1), (-1, 0)]
+        # bfs
         while q:
             size = len(q)
             for _ in range(size):
-                x1, y1 = q.popleft()
-                for x, y in directions:
+                x1, y1 = q.popleft() # 1. pull node
+                for x, y in directions: # getting all the neighbors
                     x2, y2 = x1 + x, y1 + y
+                    # check if it's valid & hasn't been visitted yet
                     if x2 >= 0 and x2 < m and y2 >= 0 and y2 < n and (x2, y2) not in visitted:
-                        matrix[x2][y2] = matrix[x1][y1] + 1
-                        q.append((x2, y2))
+                        matrix[x2][y2] = matrix[x1][y1] + 1 # 2. process node
+                        q.append((x2, y2)) # 3. process its children
                         visitted.add((x2, y2))
         return matrix
-                        
-        
-        
