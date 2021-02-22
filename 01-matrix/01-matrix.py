5f814b3d4788793a38a799baec194663ba3c8fce
Ryan Efendy <ryanefendy95@gmail.com>
2021-02-22 15:01:03 -0800

    Time: 684 ms (65.93%), Space: 17.2 MB (60.56%) - LeetHub

--- 01-matrix/01-matrix.py
+++ 01-matrix/01-matrix.py
@@ -15,14 +15,14 @@ class Solution:
         directions = [(0,1), (1,0), (0,-1), (-1, 0)]
         # bfs
         while q:
-            size = len(q)
-            for _ in range(size):
-                x1, y1 = q.popleft() # 1. pull node
-                for x, y in directions: # getting all the neighbors
-                    x2, y2 = x1 + x, y1 + y
-                    # check if it's valid & hasn't been visitted yet
-                    if x2 >= 0 and x2 < m and y2 >= 0 and y2 < n and (x2, y2) not in visitted:
-                        matrix[x2][y2] = matrix[x1][y1] + 1 # 2. process node
-                        q.append((x2, y2)) # 3. process its children
-                        visitted.add((x2, y2))
+            # size = len(q)
+            # for _ in range(size):
+            x1, y1 = q.popleft() # 1. pull node
+            for x, y in directions: # getting all the neighbors
+                x2, y2 = x1 + x, y1 + y
+                # check if it's valid & hasn't been visitted yet
+                if x2 >= 0 and x2 < m and y2 >= 0 and y2 < n and (x2, y2) not in visitted:
+                    matrix[x2][y2] = matrix[x1][y1] + 1 # 2. process node
+                    q.append((x2, y2)) # 3. process its children
+                    visitted.add((x2, y2))
         return matrix
