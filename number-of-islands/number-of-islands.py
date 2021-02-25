327e138a8faf0b91397285cbd95a30576268c952
Ryan Efendy <ryanefendy95@gmail.com>
2021-02-25 14:05:27 -0800

    Time: 160 ms (32.24%), Space: 19.2 MB (15.90%) - LeetHub

--- number-of-islands/number-of-islands.py
+++ number-of-islands/number-of-islands.py
@@ -6,7 +6,7 @@ class Solution:
     
     def numIslands(self, grid: List[List[str]]) -> int:        
         if not grid or not len(grid) or not len(grid[0]): return 0
-        self.q = collections.deque()
+        # self.q = collections.deque()
         self.visitted = set()
         self.res = 0
         m, n = len(grid), len(grid[0])
@@ -14,21 +14,30 @@ class Solution:
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == '1' and (i, j) not in self.visitted:
-                    self.q.append((i, j))
-                    self.bfs(grid, i, j)
+                    # self.q.append((i, j))
+                    # self.bfs(grid, i, j)
+                    self.dfs(grid, i, j)
                     self.res += 1
         return self.res
-        
-        
-    def bfs(self, grid, i, j):
-        m, n = len(grid), len(grid[0])
-        while self.q:
-            i, j = self.q.popleft()
-            for direction in self.directions:
-                x, y = i + direction[0], j + direction[1]
-                if x >= 0 and x < m and y >= 0 and y < n and (x, y) not in self.visitted and grid[x][y] == '1':
-                    self.q.append((x, y))
-                    self.visitted.add((x, y))
+    
+    def dfs(self, grid, i, j):
+        # grid[i][j] = 0
+        self.visitted.add((i,j))
+        for dx, dy in self.directions:
+            x = i + dx
+            y = j + dy
+            if 0 <= x < len(grid) and 0 <= y < len(grid[0]) and (x,y) not in self.visitted and grid[x][y] == '1':
+                self.dfs(grid, x, y)
+
+    # def bfs(self, grid, i, j):
+    #     m, n = len(grid), len(grid[0])
+    #     while self.q:
+    #         i, j = self.q.pop()
+    #         for direction in self.directions:
+    #             x, y = i + direction[0], j + direction[1]
+    #             if x >= 0 and x < m and y >= 0 and y < n and (x, y) not in self.visitted and grid[x][y] == '1':
+    #                 self.q.append((x, y))
+    #                 self.visitted.add((x, y))
 
                     
                         
