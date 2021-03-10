af8e61ebdf1989b1178d069801cfca920af1c85e
Ryan Efendy <ryanefendy95@gmail.com>
2021-03-10 08:49:28 -0800

    Time: 44 ms (41.55%), Space: 14.5 MB (46.67%) - LeetHub

--- permutations/permutations.py
+++ permutations/permutations.py
@@ -1,11 +1,18 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         res = []
-        self.backtrack(nums, [], res)
+        visitted = set()
+        self.backtrack(nums, [], visitted, res)
         return res
         
-    def backtrack(self, nums, path, res):
-        if not nums:
-            res.append(path)
+    def backtrack(self, nums, path, visitted, res):
+        if len(path) == len(nums):
+            res.append(path[:])
+            return
+        
         for i in range(len(nums)):
-            self.backtrack(nums[:i] + nums[i+1:], path + [nums[i]], res)
+            if i not in visitted:
+                visitted.add(i)    
+                self.backtrack(nums, path + [nums[i]], visitted, res)
+                visitted.remove(i)
+                
