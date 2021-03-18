c704488c3e80989541a13429e56c2357a4d6f062
Ryan Efendy <ryanefendy95@gmail.com>
2021-03-17 17:59:19 -0700

    Time: 28 ms (90.42%), Space: 14.2 MB (71.55%) - LeetHub

--- number-of-1-bits/number-of-1-bits.py
+++ number-of-1-bits/number-of-1-bits.py
@@ -1,5 +1,14 @@
 class Solution:
+    # flip the least significant bit
     def hammingWeight(self, n: int) -> int:
+        count = 0
+        while n:
+            n &= n-1 
+            count += 1
+        return count
+    
+    # count each bit O(32) ~ O(1)
+    def hammingWeight2(self, n: int) -> int:
         count = 0
         mask = 1
         while n:
