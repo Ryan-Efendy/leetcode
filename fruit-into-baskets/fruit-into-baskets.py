fce696ddaad47b05757ea1f48f835b16000dc9b4
Ryan Efendy <ryanefendy95@gmail.com>
2021-04-06 06:49:42 -0700

    Time: 864 ms (40.79%), Space: 20.6 MB (5.65%) - LeetHub

--- fruit-into-baskets/fruit-into-baskets.py
+++ fruit-into-baskets/fruit-into-baskets.py
@@ -1,14 +1,22 @@
 class Solution:
-    def totalFruit(self, tree: List[int]) -> int:
-        max_fruits = 0
-        i, j = 0, 0
-        d = collections.defaultdict(int)
-        while j < len(tree):
-            d[tree[j]] += 1
-            j += 1
-            while len(d) > 2:
-                d[tree[i]] -= 1
-                if not d[tree[i]]: d.pop(tree[i], None)
-                i += 1
-            max_fruits = max(max_fruits, j-i)
-        return max_fruits
+    def totalFruit(self, tree: List[int]) -> int:
+        '''
+        calculate max window w/ at most 2 diff chars
+        '''
+        counter = defaultdict(int)
+
+        i, j, res = 0, 0, 0
+        
+        while j < len(tree):
+            counter[tree[j]] += 1
+            
+            while len(counter) > 2:
+                counter[tree[i]] -= 1
+                if counter[tree[i]] == 0:
+                    del counter[tree[i]]
+                i += 1
+                
+            res = max(res, j - i + 1)
+            j += 1
+            
+        return res
