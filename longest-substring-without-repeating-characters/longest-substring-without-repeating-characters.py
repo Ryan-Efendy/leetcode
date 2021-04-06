8474684a39509a89e9aa6ad671ad6e43aaf6968f
Ryan Efendy <ryanefendy95@gmail.com>
2021-04-06 11:42:01 -0700

    Time: 72 ms (43.93%), Space: 14.3 MB (54.84%) - LeetHub

--- longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,20 +1,17 @@
 class Solution:
-    '''
-    s = "abcabcbb"
-            j
-          i
-    '''
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        max_len, i, j = 0, 0, 0
-        hash_set = set()
-        while j < len(s):
-            while s[j] in hash_set:
-                hash_set.remove(s[i])
-                i += 1   
-            hash_set.add(s[j])
-            j += 1
-            max_len = max(max_len, j-i)
-        return max_len
-        
-        
-        
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if len(s) == 0 or not s: return 0
+        counter = collections.defaultdict(int)
+        left = 0
+        res = 0
+        for right, ch in enumerate(s):
+            # counter[ch] += 1
+            while ch in counter:
+                counter[s[left]] -= 1
+                if counter[s[left]] == 0:
+                    del counter[s[left]]
+                left += 1
+            
+            counter[ch] += 1
+            res = max(res, right - left + 1)
+        return res
