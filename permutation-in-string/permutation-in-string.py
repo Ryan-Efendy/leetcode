739c9926ed9592fe82d0968fe96f073d67feb71e
Ryan Efendy <ryanefendy95@gmail.com>
2021-04-06 19:38:31 -0700

    Time: 108 ms (34.37%), Space: 14.4 MB (34.52%) - LeetHub

--- permutation-in-string/permutation-in-string.py
+++ permutation-in-string/permutation-in-string.py
@@ -1,24 +1,19 @@
 class Solution:
-    '''
-    01234567
-    eidboaoo
-    
-    '''
-    def checkInclusion(self, s1: str, s2: str) -> bool:
-        counter = collections.Counter(s1)
-        i, j = 0, 0
-        count, res = len(counter), 0
-        while j < len(s2):
-            if s2[j] in counter:
-                counter[s2[j]] -= 1
-                if counter[s2[j]] == 0:
-                    count -= 1
-            j += 1
-            if count == 0: return True
-            if j - i > len(s1)-1:
-                if s2[i] in counter:
-                    if counter[s2[i]] == 0:
-                        count += 1
-                    counter[s2[i]] += 1
-                i += 1
-        return False
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        i = 0
+        minLen = math.inf
+        counter, count = collections.Counter(s1), 0
+
+        for j, ch in enumerate(s2):
+            counter[ch] -= 1
+            if counter[ch] >= 0: # ch is in t
+                count += 1
+
+            while count == len(s1):
+                minLen = min(minLen, j - i + 1)
+                counter[s2[i]] += 1
+                if counter[s2[i]] > 0: # ch is in t
+                    count -= 1
+                i += 1
+        
+        return minLen == len(s1)
