1313f9b005811923720136cc228c528a6175fd19
Ryan Efendy <ryanefendy95@gmail.com>
2021-04-06 17:02:24 -0700

    Time: 460 ms (19.15%), Space: 27.6 MB (67.24%) - LeetHub

--- maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.py
+++ maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.py
@@ -1,7 +1,10 @@
 class Solution:
     def maxScore(self, cardPoints: List[int], k: int) -> int:
         '''
+        O(N) Time, O(1) Space
+        running sum instead of presum
         k = 3 | n-k = 5
+        01234567
         XXXXXXXX sliding window 000
         XXXXX000 instead of finding max score, find min score & subtract from total
         0XXXXX00
@@ -9,13 +12,40 @@ class Solution:
         000XXXXX
         '''
         n = len(cardPoints)
-        pre_sums = [0]
-        for cardPoint in cardPoints:
-            pre_sums.append(cardPoint+pre_sums[-1])
+        running_sum = 0
+        left = right = 0
+        min_sum = math.inf
 
-        min_array_sum = math.inf
-        for left in range(k+1):
-            # A[i, j] = A[j] - A[i]
-            min_array_sum = min(min_array_sum, pre_sums[left+n-k] - pre_sums[left])
+        for right, num in enumerate(cardPoints):
+            running_sum += num
 
-        return pre_sums[-1] - min_array_sum
+            if right - left > n-k-1:
+                running_sum -= cardPoints[left]
+                left += 1
+
+            if right - left == n-k-1:
+                min_sum = min(min_sum, running_sum)
+
+        return sum(cardPoints) - min_sum
+
+#     def maxScore(self, cardPoints: List[int], k: int) -> int:
+#         '''
+#         O(N) Time & Space
+#         k = 3 | n-k = 5
+#         XXXXXXXX sliding window 000
+#         XXXXX000 instead of finding max score, find min score & subtract from total
+#         0XXXXX00
+#         00XXXXX0
+#         000XXXXX
+#         '''
+#         n = len(cardPoints)
+#         pre_sums = [0]
+#         for cardPoint in cardPoints:
+#             pre_sums.append(cardPoint+pre_sums[-1])
+
+#         min_array_sum = math.inf
+#         for i in range(k+1):
+#             # A[i, j] = A[j] - A[i]
+#             min_array_sum = min(min_array_sum, pre_sums[i+n-k] - pre_sums[i])
+
+#         return pre_sums[-1] - min_array_sum
