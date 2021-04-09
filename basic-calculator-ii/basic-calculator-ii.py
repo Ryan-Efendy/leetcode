d9be85e61715e1be9f71154df67245d2ffc7e943
Ryan Efendy <ryanefendy95@gmail.com>
2021-04-08 22:06:39 -0700

    Time: 80 ms (75.00%), Space: 15.6 MB (76.50%) - LeetHub

--- basic-calculator-ii/basic-calculator-ii.py
+++ basic-calculator-ii/basic-calculator-ii.py
@@ -1,23 +1,32 @@
 class Solution:
     def calculate(self, s: str) -> int:
+        '''
+        1. if the curr op is +|- then the exp is eval based on the precedence of next op.
+        2. if the curr op is *|/ then the exp is eval irrespective of next op
+        '''
         if not s: return 0
 
         stack, curr_num, operator = [], 0, '+'
         operators = {'+', '-', '*', '/'}
-        nums = set(str(i) for i in range(10))
+        operands = set(str(i) for i in range(10))
+
         for i in range(len(s)):
             char = s[i]
-
-            if char in nums:
+            # if char is operand (0-9) add it to curr_num
+            if char in operands:
                 curr_num = curr_num * 10 + int(char)
 
             if char in operators or i == len(s)-1:
+                # 1. +/- have lower precedent, eval later based on next operation
+                # push to curr_num stack to be eval later based on next op
                 if operator == '+':
                     stack.append(curr_num)
 
                 elif operator == '-':
                     stack.append(-curr_num)
 
+                # 2. */- highest precent eval right away
+                # pop top of stack and eval right away
                 elif operator == '*':
                     stack[-1] *= curr_num
 
@@ -26,5 +35,5 @@ class Solution:
 
                 curr_num = 0
                 operator = char
-                
+
         return sum(stack)
