305c1b63272208bf35652e512a186440a788967a
Ryan Efendy <ryanefendy95@gmail.com>
2021-04-08 21:56:10 -0700

    Time: 36 ms (88.12%), Space: 14.3 MB (80.89%) - LeetHub

--- basic-calculator-iii/basic-calculator-iii.py
+++ basic-calculator-iii/basic-calculator-iii.py
@@ -1,5 +1,19 @@
 class Solution:
     def calculate(self, s: str) -> int:
+        '''
+        https://leetcode.com/problems/basic-calculator-iii/discuss/276478/Python-one-simple-template-solving-basic-calculator-I-and-III
+
+        1.Using res to keep track of the current calculated result.
+        2.Using op to keep track of the operator.
+        3.Using num to keep track of the current number used to update res.
+        4.If needed, using prev to keep track of previous number added to the res.
+        5.Go through each char in the string
+            5.1.if meet (, push res, op, prev, to stack.
+            5.2 if meet ), update res, pop whatever is in the stack
+            5.3 if meet operator, update res
+            5.4 if meet digit, update num
+            Small trick: add an extra + to the end of string to make sure the entire string is evaluated.
+        '''
         def update(a, b, prev, op):
             if op == '-':
                 b *= -1
