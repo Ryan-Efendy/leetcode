1d99275b0aab701d54844fa205ea31ea2bd2fd12
Ryan Efendy <ryanefendy95@gmail.com>
2021-04-12 13:59:04 -0700

    Time: 252 ms (76.08%), Space: 16.8 MB (60.45%) - LeetHub

--- subarray-sum-equals-k/subarray-sum-equals-k.py
+++ subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,17 +1,17 @@
-from itertools import accumulate
-from collections import defaultdict
-
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
+        '''
+        cumulative sum + two sum using/ hashmap O(n) Time & Space
+        '''
         d = defaultdict(int)
-        d[0] += 1
+        d[0] += 1 # b/c itertools.accumulate will have the same size as nums, need to init
         presums = list(accumulate(nums))
         res = 0
-    
+
         for presum in presums:
             diff = presum - k
             if diff in d:
                 res += d[diff]
             d[presum] += 1
-            
+
         return res
