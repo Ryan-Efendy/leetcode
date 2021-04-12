62ed9c4309ac53000f699b3911b4f90c65f549aa
Ryan Efendy <ryanefendy95@gmail.com>
2021-04-11 21:00:39 -0700

    Time: 256 ms (63.10%), Space: 16.8 MB (60.42%) - LeetHub

--- subarray-sum-equals-k/subarray-sum-equals-k.py
+++ subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,23 +1,17 @@
+from itertools import accumulate
+from collections import defaultdict
+
 class Solution:
-    '''
-    nums = [3,4,7,2,-3,1,4,2] k=7
-           [3,7,14,16,13,14,18,20]
-              i 
-    nums = [1,2,3], k = 3
-           [1,3,6]
-    nums = [1,1,1], k = 2
-           [1,2,3]
-    '''
-    def subarraySum(self, nums: List[int], k: int) -> int:
-        prefix = list(accumulate(nums))
-        
-        res = 0
-        d = defaultdict(int)
-        d[0] = 1
-        for i in range(len(prefix)):
-            if prefix[i]-k in d: res += d[prefix[i]-k]
-            d[prefix[i]] += 1
-        return res
-            
-                
-             
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        d = defaultdict(int)
+        d[0] += 1
+        presums = list(accumulate(nums))
+        res = 0
+    
+        for presum in presums:
+            diff = presum - k
+            if diff in d:
+                res += d[diff]
+            d[presum] += 1
+            
+        return res
