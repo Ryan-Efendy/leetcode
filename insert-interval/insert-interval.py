d3ffa024a372dc48823a6608bf3415fabaf042aa
Ryan Efendy <ryanefendy95@gmail.com>
2021-04-17 08:25:22 -0700

    Time: 100 ms (5.28%), Space: 17.5 MB (29.14%) - LeetHub

--- insert-interval/insert-interval.py
+++ insert-interval/insert-interval.py
@@ -1,17 +1,30 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        intervals.append(newInterval)
-        return self.merge(intervals)
-    
-    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals.sort(key=lambda interval: interval[0]) 
-
-        merged_intervals = [intervals[0]]
-
+        '''
+        Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+        
+        res: [[1,5],[6,9]]
+        
+        1. add non-overlapping intervals to newInterval
+        2. merge newInterval w/ overlapping intervals
+        3. add remaining non-overlapping intervals
+        '''
+        res = []
+        
         for interval in intervals:
-            if interval[0] <= merged_intervals[-1][1]:
-                merged_intervals[-1][1] = max(merged_intervals[-1][1], interval[1])
+            # 1
+            if interval[1] < newInterval[0]:
+                res.append(interval)
+            # 3        
+            elif interval[0] > newInterval[1]:
+                res.append(newInterval)
+                newInterval = interval
+            # 2
             else:
-                merged_intervals.append(interval)
-
-        return merged_intervals
+                newInterval[0] = min(interval[0], newInterval[0])
+                newInterval[1] = max(interval[1], newInterval[1])
+                    
+        res.append(newInterval)
+        return res
+        
+        
