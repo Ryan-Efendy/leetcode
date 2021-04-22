f88be66c4102fdca0643b9bf376a55f406b0791d
Ryan Efendy <ryanefendy95@gmail.com>
2021-04-22 13:20:18 -0700

    Time: 44 ms (71.74%), Space: 20 MB (5.30%) - LeetHub

--- path-sum-ii/path-sum-ii.py
+++ path-sum-ii/path-sum-ii.py
@@ -7,15 +7,16 @@
 class Solution:
     def pathSum(self, root: TreeNode, targetSum: int) -> List[List[int]]:
         res = []
-        def helper(root: TreeNode, targetSum: int, paths: List[int]):
-            if not root:
-                return False
-            paths.append(root.val)
-            if not root.left and not root.right and targetSum - root.val == 0:
-                res.append(paths)
+        def helper(root: TreeNode, targetSum: int, path: List[int]):
+            if not root: return
+            
+            path.append(root.val)
+            if not root.left and not root.right and root.val == targetSum:
+                res.append(path)
                 return
-            helper(root.left, targetSum - root.val, paths[:])
-            helper(root.right, targetSum - root.val, paths[:])
+            
+            helper(root.left, targetSum - root.val, path[:])
+            helper(root.right, targetSum - root.val, path[:])
         
         helper(root, targetSum, [])
         return res
