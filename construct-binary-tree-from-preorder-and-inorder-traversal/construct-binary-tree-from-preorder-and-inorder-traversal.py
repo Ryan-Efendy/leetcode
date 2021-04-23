f97c5ce1ab5b66d8153720bbca80b821bf600f5d
Ryan Efendy <ryanefendy95@gmail.com>
2021-04-23 15:05:54 -0700

    Time: 196 ms (25.86%), Space: 88.5 MB (14.97%) - LeetHub

--- construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -6,21 +6,20 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        '''
+        https://www.youtube.com/watch?v=ihj4IQGZ2zc
+        
+        1. the first val in preorder -> root
+           [3, 9, 20, 15, 7]
+            \U0001f446 
+        2. use root to partition the left & right subtree in inorder
+           [9, 3, 15, 20, 7]
+left subtree <-\U0001f446  -> right subtree    
+        '''
         if not preorder or not inorder: return None
-        map_inorder = {}
-        for i, val in enumerate(inorder): map_inorder[val] = i
-            
-        def helper(preorder, p1, p2, inorder, i1, i2):
-            if p1 >= p2 or i1 >= i2: return None
-            root = TreeNode(preorder[p1])
-            # idx = inorder.index(root.val)
-            idx = map_inorder[root.val]
-            delta = idx - i1
 
-            root.left = helper(preorder, p1+1, p1+1+delta,
-                                inorder, i1, i1+delta)
-            root.right = helper(preorder, p1+delta+1, p2,
-                                inorder, i1+delta+1, i2)
-            return root
-            
-        return helper(preorder, 0, len(preorder), inorder, 0, len(inorder))
+        root = TreeNode(preorder[0])
+        mid = inorder.index(preorder[0])
+        root.left = self.buildTree(preorder[1:mid + 1], inorder[:mid])
+        root.right = self.buildTree(preorder[mid + 1:], inorder[mid+1:])
+        return root
