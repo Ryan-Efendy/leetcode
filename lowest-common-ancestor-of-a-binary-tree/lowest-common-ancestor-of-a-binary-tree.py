910745a96d498a2edde8fc9b124bf74027782b12
Ryan Efendy <ryanefendy95@gmail.com>
2021-04-23 13:11:42 -0700

    Time: 68 ms (81.77%), Space: 27.3 MB (35.86%) - LeetHub

--- lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -7,11 +7,14 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if not root: return root
-        if root.val == p.val or root.val == q.val: return root
-        left = self.lowestCommonAncestor(root.left, p, q)
-        right = self.lowestCommonAncestor(root.right, p, q)
-        if not left: return right
-        if not right: return left
-        return root
+        def dfs(root):
+            if not root: return None
+            # if both returns a valid node -> p, q are in different subtrees, then root will be their LCA.
+            if root == p or root == q: return root
+            left = dfs(root.left)        
+            right = dfs(root.right)
+            if left and right: return root
+            # if only one valid node returns, which means p, q are in the same subtree, return that valid node as their LCA.
+            return left if left else right
         
+        return dfs(root)
