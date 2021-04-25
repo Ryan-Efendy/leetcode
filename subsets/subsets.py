685eb836068a040fb7b85662330859f053aa0806
Ryan Efendy <ryanefendy95@gmail.com>
2021-04-25 13:57:07 -0700

    Time: 32 ms (77.15%), Space: 14.3 MB (92.10%) - LeetHub

--- subsets/subsets.py
+++ subsets/subsets.py
@@ -1,14 +1,36 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        self.backtrack(nums, 0, [], res)
-        return res
+        '''
+        nums = [1,2,3]
+                        [ ] ✅
+                      /  |  \  
+                    1    2    3
+                  [1]✅  [2]✅  [3]✅
+                 /  \      \   
+                2    3       3
+            [1,2]✅ [1,3]✅  [2,3]✅
+               |
+               3
+            [1,2,3]✅ 
         
-    def backtrack(self, nums, i, path, res):
-        if i >= len(nums):
-            res.append(path[:])
-            return
-        path.append(nums[i])
-        self.backtrack(nums, i+1, path, res)
-        path.pop()
-        self.backtrack(nums, i+1, path, res)
+        Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+        
+        '''
+        res = []
+        subset = []
+        
+        def dfs(i):
+            if i >= len(nums):
+                res.append(subset.copy())
+                return
+
+            # decision to include nums[i]
+            subset.append(nums[i])
+            dfs(i + 1)
+
+            # decision NOT to include nums[i]
+            subset.pop()
+            dfs(i + 1)
+
+        dfs(0)
+        return res
