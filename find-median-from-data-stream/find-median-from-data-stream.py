58ab4112653b888e8aa599948516459e8344555e
Ryan Efendy <ryanefendy95@gmail.com>
2021-04-25 09:38:17 -0700

    Time: 188 ms (76.74%), Space: 26 MB (12.10%) - LeetHub

--- find-median-from-data-stream/find-median-from-data-stream.py
+++ find-median-from-data-stream/find-median-from-data-stream.py
@@ -2,60 +2,66 @@ from heapq import *
 
 class MedianFinder:
     '''
-    arr = [1,3,5,4,7,8]         1. insert
-           \U0001f446                   2. balance 
-     SMALL       LARGE      
-     maxpq       minpq
-    -------     ------- 
-        1
-    -------     -------
-     SMALL       LARGE  
-     maxpq       minpq     arr = [1,3,5,4,7,8]  
-    -------     -------             \U0001f446
-        1       3
-    -------     -------
-     SMALL       LARGE  
-     maxpq       minpq     arr = [1,3,5,4,7,8]  
-    -------     -------               \U0001f446
-         1      3 5 ❌ \U0001f448 minpq.pop() -> maxpq.push(3)
-    -------     -------
-    -------     -------             
-       1  3     5
-    -------     -------
-     SMALL       LARGE  
-     maxpq       minpq     arr = [1,3,5,4,7,8]  
-    -------     -------                 \U0001f446
-        1 3     4 5
-    -------     -------
-     SMALL       LARGE  
-     maxpq       minpq     arr = [1,3,5,4,7,8]  
-    -------     -------                   \U0001f446
-        1 3     4 5 7 ❌ minpq.pop() -> maxpq.push(4)
-    -------     -------
-    -------     -------             
-    1  3  4     5 7
-    -------     -------    
+        One minheap to store low values and second maxheap to store max values, we keep track and update median every time after insertion
+		TIME COMPLEXITY : O(logN) | SPACE COMPLEXITY : O(N)
+        
+        Adding number 41
+        MaxHeap lo: [41]           // MaxHeap stores the largest value at the top (index 0)
+        MinHeap hi: []             // MinHeap stores the smallest value at the top (index 0)
+        Median is 41
+        =======================
+        Adding number 35
+        MaxHeap lo: [35]          // max heap stores smaller half of nums
+        MinHeap hi: [41]          // min heap stores bigger half of nums
+        Median is 38
+        =======================
+        Adding number 62
+        MaxHeap lo: [41, 35]
+        MinHeap hi: [62]
+        Median is 41
+        =======================
+        Adding number 4
+        MaxHeap lo: [35, 4]
+        MinHeap hi: [41, 62]
+        Median is 38
+        =======================
+        Adding number 97
+        MaxHeap lo: [41, 35, 4]
+        MinHeap hi: [62, 97]
+        Median is 41
+        =======================
+        Adding number 108
+        MaxHeap lo: [41, 35, 4]
+        MinHeap hi: [62, 97, 108]
+        Median is 51.5
     '''
     def __init__(self):
         """
         initialize your data structure here.
         """
-        self.small = [] # the smaller half of the list, MAX heap (invert min-heap)
-        self.large = [] # the larger half of the list, MIN heap
+        self.maxHeap = []  # containing first half of numbers
+        self.minHeap = []  # containing second half of numbers
         
 
-    def addNum(self, num: int) -> None:
-        if len(self.small) == len(self.large):
-            heappush(self.large, -heappushpop(self.small, -num))
+    def addNum(self, num):
+        if not self.maxHeap or -self.maxHeap[0] >= num:
+            heappush(self.maxHeap, -num)
         else:
-            heappush(self.small, -heappushpop(self.large, num))
-        
-    def findMedian(self) -> float:
-        # if even
-        if len(self.large) == len(self.small):
-            return (self.large[0] - self.small[0])/2
-        # return -self.small[0] if len(self.small) > len(self.large) else self.large[0]
-        return self.large[0]
+            heappush(self.minHeap, num)
+
+        # either both the heaps will have equal number of elements or max-heap will have one
+        # more element than the min-heap
+        if len(self.maxHeap) > len(self.minHeap) + 1:
+            heappush(self.minHeap, -heappop(self.maxHeap))
+        elif len(self.maxHeap) < len(self.minHeap):
+            heappush(self.maxHeap, -heappop(self.minHeap))
+            
+    def findMedian(self):
+        if len(self.maxHeap) == len(self.minHeap):
+        # we have even number of elements, take the average of middle two elements
+            return -self.maxHeap[0] / 2.0 + self.minHeap[0] / 2.0
+        # because max-heap will have one more element than the min-heap
+        return -self.maxHeap[0] / 1.0
 
 
 # Your MedianFinder object will be instantiated and called as such:
