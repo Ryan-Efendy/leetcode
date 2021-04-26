b6b67be7c51c2c2eb9fa04916705a86c3ebf3a61
Ryan Efendy <ryanefendy95@gmail.com>
2021-04-25 19:22:53 -0700

    Time: 500 ms (46.51%), Space: 15.9 MB (15.47%) - LeetHub

--- combinations/combinations.py
+++ combinations/combinations.py
@@ -1,19 +1,28 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
+        '''
+        Input: n = 4, k = 2
+        Output:
+        [
+          [2,4],
+          [3,4],
+          [2,3],
+          [1,2],
+          [1,3],
+          [1,4],
+        ]
+        '''
         res = []
-        if k == 0: return res
-        self.backtrack(1, n, k, [], res)
+        nums = [i for i in range(1, n+1)]
+
+        def backtrack(path, i):
+            if len(path) == k:
+                res.append(path[:])
+
+            for j in range(i, len(nums)): # choices
+                path.append(nums[j]) # choose
+                backtrack(path, j+1) # explore
+                path.pop()           # unchoose 
+
+        backtrack([], 0)
         return res
-        
-    def backtrack(self, start, n, k, path, res):
-        if len(path) == k:
-            res.append(path[:])
-        
-        for i in range(start, n+1):
-            if len(path) < k:
-                path.append(i)
-                self.backtrack(i+1, n, k, path, res)
-                path.pop()
-                
-        
-        
