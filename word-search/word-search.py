c775cb785e32482ef72ae7e5cb022510624f0d28
Ryan Efendy <ryanefendy95@gmail.com>
2021-04-26 11:18:39 -0700

    Time: 3712 ms (61.06%), Space: 14.2 MB (95.94%) - LeetHub

--- word-search/word-search.py
+++ word-search/word-search.py
@@ -1,18 +1,22 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         m, n = len(board), len(board[0])
-        visited = set() 
+        # visited = set() 
         directions = [(1,0), (0,1), (-1,0), (0,-1)]
 
         def dfs(r, c, i):
-            if i == len(word):
+            if i == len(word): # 3. goal reached
                 return True
-            if 0 <= r < m and 0 <= c < n and word[i] == board[r][c] and (r,c) not in visited:
-                visited.add((r, c))
-                for dx, dy in directions:
-                    if dfs(r + dx, c + dy, i + 1):
-                        return True
-                visited.remove((r, c))
+            # 2. Constraint 
+            if 0 <= r < m and 0 <= c < n and word[i] == board[r][c]:
+                # visited.add((r, c)) # a. choose
+                ch = board[r][c]
+                board[r][c] = '#'
+                for dx, dy in directions: # 1. Choices
+                    if dfs(r + dx, c + dy, i + 1): # b. explore
+                        return True 
+                # visited.remove((r, c)) # c. unchoose
+                board[r][c] = ch
             return False
 
         for r in range(m):
