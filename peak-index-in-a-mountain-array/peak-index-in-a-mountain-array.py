e0abda0bf16fdbd46b2821393014a3d9c392fdad
Ryan Efendy <ryanefendy95@gmail.com>
2021-04-26 21:55:38 -0700

    Time: 76 ms (50.78%), Space: 15.4 MB (54.52%) - LeetHub

--- peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.py
+++ peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.py
@@ -1,21 +1,19 @@
 class Solution:
-    def peakIndexInMountainArray(self, nums: List[int]) -> int:
-        '''
-         0,1,2,3
-        [0,2,1,0]
-         l 
-         m r
-             
-         0,1,2,3,4,5,6,7,8,9,10    
-        [0,1,2,3,4,5,6,5,4,3,2]
-                     l  
-                     m r         
-        '''
-        l, r = 0, len(nums)-1
-        while l < r:
-            m = l + (r-l)//2
-            if nums[m] > nums[m+1]:
-                r = m
-            else:
-                l = m + 1
-        return l
+    def peakIndexInMountainArray(self, nums: List[int]) -> int:
+        '''
+          /\
+         /  \ 
+        /    \
+        0123456
+           \U0001f446 
+        0001111
+        f(i) = arr[i] > arr[i+1]
+        '''
+        l, r = 0, len(nums)-1
+        while l < r:
+            m = l + (r-l)//2
+            if nums[m] < nums[m+1]:
+                l = m + 1
+            else:
+                r = m
+        return l
