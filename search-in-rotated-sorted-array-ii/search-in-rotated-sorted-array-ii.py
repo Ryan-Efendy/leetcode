dddd6455e61322aae4d3c24c67e9218703c10dad
Ryan Efendy <ryanefendy95@gmail.com>
2021-04-26 19:34:35 -0700

    Time: 52 ms (70.31%), Space: 15.1 MB (32.26%) - LeetHub

--- search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
+++ search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
@@ -1,34 +1,33 @@
 class Solution:
-    def search(self, nums: List[int], target: int) -> bool:
-        if len(nums) == 0: return False
-        n = len(nums)
-        l, r, pivot = 0, n-1, None
-        while l < r:
-            m = l + (r-l)//2
-            if nums[m] == nums[r]:
-                # find the minimum value index vs. just minimum value
-                if (nums[r] < nums[r-1]):
-                    pivot = r
-                    break
-                r -= 1
-            elif nums[m] > nums[r]:
-                l = m + 1
-            else:    
-                r = m
-        
-        if not pivot:
-            pivot = l
-        
-        l, r = 0, n-1
-        if nums[pivot] <= target <= nums[r]:
-            l = pivot
-        else:
-            r = pivot
-        while l < r:
-            m = l + (r-l)//2
-            if nums[m] < target:
-                l = m + 1
-            else:
-                r = m
-        return True if nums[l] == target else False
-            
+    def search(self, nums: List[int], target: int) -> bool:
+        def findMin(nums: List[int]) -> int:
+            lo, hi, pivot = 0, len(nums)-1, None
+            while lo < hi:
+                mid = lo + (hi-lo)//2
+                if nums[mid] == nums[hi]:
+                    # find the minimum value index vs. just minimum value
+                    if (nums[hi] < nums[hi-1]):
+                        pivot = hi
+                        break
+                    hi -= 1
+                elif nums[mid] > nums[hi]:
+                    lo = mid + 1
+                else:    
+                    hi = mid
+            return pivot if pivot else lo
+        
+        def search(nums: List[int], target: int, rotation: int) -> int:
+            lo, hi = 0, len(nums)
+            while lo < hi:
+                mid = (lo + hi)//2
+                midPlusRotation = (mid + rotation) % len(nums)
+                if nums[midPlusRotation] < target:
+                    lo = mid + 1
+                else:
+                    hi = mid
+            lo = (lo + rotation) % len(nums) # This is the original LEFT index
+            return nums[lo] == target
+        
+        # Normalize: left - rotation = 0
+        rotation = findMin(nums)
+        return search(nums, target, rotation)
