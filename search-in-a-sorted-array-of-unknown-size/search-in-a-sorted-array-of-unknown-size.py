bf67850189ad66daf6e5d45b7f05302326e6e3f0
Ryan Efendy <ryanefendy95@gmail.com>
2021-04-27 08:02:47 -0700

    Time: 28 ms (94.28%), Space: 15.3 MB (78.47%) - LeetHub

--- search-in-a-sorted-array-of-unknown-size/search-in-a-sorted-array-of-unknown-size.py
+++ search-in-a-sorted-array-of-unknown-size/search-in-a-sorted-array-of-unknown-size.py
@@ -3,20 +3,41 @@
 # You should not implement it, or speculate about its implementation
 # """
 #class ArrayReader:
-#    def get(self, index: int) -> int:
-​
+#    def get(self, index: int) -> int:
+
 class Solution:
-    def search(self, reader, target):
-        """
-        :type reader: ArrayReader
-        :type target: int
-        :rtype: int
-        """
-        l, r = 0, 9999
-        while l < r:
-            m = l + (r-l)//2
-            if reader.get(m) < target:
-                l = m + 1
-            else:
-                r = m
-        return l if reader.get(l) == target else -1
+    def search(self, reader, target):
+        '''
+        need to find r, by increasing r exponentially -> while (reader.get(r) < target) r *= 2
+        1. find search limits, i.e. left & right boundaries for the search
+        2. do binary search in the defined boundaries
+        '''
+        """
+        :type reader: ArrayReader
+        :type target: int
+        :rtype: int
+        """
+        if reader.get(0) == target:
+            return 0
+        
+        # search boundaries
+        left, right = 0, 1
+        while reader.get(right) < target:
+            left = right
+            right *= 2
+        
+        # binary search
+        while left <= right:
+            pivot = left + ((right - left) >> 1)
+            num = reader.get(pivot)
+            
+            if num == target:
+                return pivot
+            if num > target:
+                right = pivot - 1
+            else:
+                left = pivot + 1
+        
+        # there is no target element
+        return -1
+        
