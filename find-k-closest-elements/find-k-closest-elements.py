e5cc2920d222b4feb7e706b119c7a4bcc2ea365b
Ryan Efendy <ryanefendy95@gmail.com>
2021-04-27 07:55:50 -0700

    Time: 280 ms (84.77%), Space: 15.6 MB (76.80%) - LeetHub

--- find-k-closest-elements/find-k-closest-elements.py
+++ find-k-closest-elements/find-k-closest-elements.py
@@ -1,11 +1,35 @@
 class Solution:
-    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        l, r = 0, len(arr)-k
-        while l < r:
-            m = l + (r-l)//2  # m is the start of window size k
-            if x - arr[m] > arr[m+k] - x:
-                l = m + 1
-            else:
-                r = m
-        return arr[l:l+k]
-            
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        '''
+        https://leetcode.com/problems/find-k-closest-elements/discuss/106426/JavaC%2B%2BPython-Binary-Search-O(log(N-K)-%2B-K)
+        
+        Assume A[mid] ~ A[mid + k] is sliding window
+
+        case 1: x - A[mid] < A[mid + k] - x, need to move window go left
+        -------x----A[mid]-----------------A[mid + k]----------
+
+        case 2: x - A[mid] < A[mid + k] - x, need to move window go left again
+        -------A[mid]----x-----------------A[mid + k]----------
+
+        case 3: x - A[mid] > A[mid + k] - x, need to move window go right
+        -------A[mid]------------------x---A[mid + k]----------
+
+        case 4: x - A[mid] > A[mid + k] - x, need to move window go right
+        -------A[mid]---------------------A[mid + k]----x------
+
+        If x - A[mid] > A[mid + k] - x,
+        it means A[mid + 1] ~ A[mid + k] is better than A[mid] ~ A[mid + k - 1],
+        and we have mid smaller than the right i.
+        So assign left = mid + 1.
+        '''
+        lo = 0
+        hi = len(arr) - k
+        while lo < hi:
+            mid = lo + (hi-lo) // 2
+
+            if x - arr[mid] > arr[mid+k] - x:
+                lo = mid + 1
+            else:
+                hi = mid
+
+        return arr[lo:lo+k]
