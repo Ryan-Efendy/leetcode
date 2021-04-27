c5708bab1c3515768ac8511e2a19a5628f5285a0
Ryan Efendy <ryanefendy95@gmail.com>
2021-04-27 08:15:21 -0700

    Time: 52 ms (37.36%), Space: 15.2 MB (20.88%) - LeetHub

--- search-insert-position/search-insert-position.py
+++ search-insert-position/search-insert-position.py
@@ -1,20 +1,13 @@
 class Solution:
-    '''
-     0,1,2,3 4
-    [1,3,5,6] target=2
-       l           
-     m r    
-     0,1,2,3 4
-    [1,3,5,6] target=7
-           l
-           m r
-    '''
-    def searchInsert(self, nums: List[int], target: int) -> int:
-        l, r = 0, len(nums)
-        while l < r:
-            m = l + (r-l)//2
-            if nums[m] < target:
-                l = m + 1
-            else:
-                r = m
-        return l
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        def bisect_left(nums: List[int], target: int) -> int:
+            l, r = 0, len(nums)
+            while l < r:
+                m = l + (r-l)//2
+                if nums[m] < target:
+                    l = m + 1
+                else:
+                    r = m
+            return l
+        
+        return bisect_left(nums, target)
