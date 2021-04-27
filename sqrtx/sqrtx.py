9b63c1d1543ee77a2a23ac971efb9956f81c31da
Ryan Efendy <ryanefendy95@gmail.com>
2021-04-27 08:21:12 -0700

    Time: 32 ms (79.39%), Space: 14 MB (88.79%) - LeetHub

--- sqrtx/sqrtx.py
+++ sqrtx/sqrtx.py
@@ -1,11 +1,14 @@
 class Solution:
-    def mySqrt(self, x: int) -> int:
-        if (x < 2): return x
-        l, r = 1, x
-        while l < r:
-            m = l + (r-l)//2
-            if m**2 <= x:
-                l = m + 1
-            else:
-                r = m
-        return l-1
+    def mySqrt(self, x: int) -> int:        
+        def bisect_right(x: int) -> int:
+            lo, hi = 0, x
+            while lo < hi:
+                mid = lo + (hi-lo)//2
+                if mid * mid <= x:
+                    lo = mid + 1
+                else:
+                    hi = mid
+            return lo-1
+        
+        if x == 0 or x == 1: return x
+        return bisect_right(x)
