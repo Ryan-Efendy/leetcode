b5efe4bd49bd342372e44c24cfae2355b5ecba2e
Ryan Efendy <ryanefendy95@gmail.com>
2021-04-26 20:02:01 -0700

    Time: 32 ms (44.30%), Space: 14.3 MB (44.49%) - LeetHub

--- first-bad-version/first-bad-version.py
+++ first-bad-version/first-bad-version.py
@@ -2,18 +2,24 @@
 # @param version, an integer
 # @return an integer
 # def isBadVersion(version):
-​
+
 class Solution:
-    def firstBadVersion(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        l, r = 0, n
-        while l < r:
-            m = l + (r-l)//2
-            if isBadVersion(m):
-                r = m
-            else:
-                l = m + 1
-        return l
+    def firstBadVersion(self, n: int) -> int:
+        '''
+        1 -› n
+        0000000111111
+        
+        FFFFFTT 
+        L  M  R - if mid is False search right
+
+        FFFTTTT  
+        L  M  R - if mid is True check if it's the first one else search left
+        '''
+        l, r = 0, n
+        while l < r:
+            m = l + (r-l)//2
+            if isBadVersion(m):
+                r = m
+            else:
+                l = m + 1
+        return l
