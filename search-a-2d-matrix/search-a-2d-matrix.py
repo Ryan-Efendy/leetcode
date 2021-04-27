c367ce81df184fa17dc951581698545136311774
Ryan Efendy <ryanefendy95@gmail.com>
2021-04-26 22:10:57 -0700

    Time: 36 ms (95.32%), Space: 14.7 MB (63.44%) - LeetHub

--- search-a-2d-matrix/search-a-2d-matrix.py
+++ search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,23 +1,25 @@
 class Solution:
-    '''
-    [ i
-     [1, 3, 5, 7 ],
-     [10,11,16,20],
-     [23,30,34,50]
-               j
-    ]
-    
-    '''
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool: 
-        if not len(matrix) or not len(matrix[0]): return False
-        
-        i, j = 0, len(matrix[0])-1
-        while i <= len(matrix)-1 and j >= 0:
-            if matrix[i][j] == target:
-                return True
-            elif matrix[i][j] < target:
-                i += 1
-            else:
-                j -= 1
-        return False
-        
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        '''
+        transform 2d matrix m x n -> 1d sorted array of length m x n
+        m x n matrix \U0001f449\U0001f3fc  to an array => matrix[x][y] =>  a[x * m + y]
+        an array     \U0001f449\U0001f3fc  to n x m matrix => a[x] => matrix[x / m][x % m]
+        
+        '''
+        if not matrix or not matrix[0]:
+            return False
+        rows, cols = len(matrix), len(matrix[0])
+        l , h = 0 , rows * cols -1        
+        while l <= h:
+            m = (l + h) //2
+            # i, j = mid//len(matrix[0]), mid%len(matrix[0])
+            r,c = divmod(m,cols)            
+            num = matrix[r][c]            
+            if num == target:
+                return True
+            elif num < target:
+                l = m + 1  
+            else:
+                h = m - 1
+        return False
+        
