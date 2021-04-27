c267bddaff63b7944a64b49be0bc4e8bb6c5fc92
Ryan Efendy <ryanefendy95@gmail.com>
2021-04-26 17:34:26 -0700

    Time: 40 ms (65.25%), Space: 14.5 MB (84.28%) - LeetHub

--- find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -1,28 +1,23 @@
 class Solution:
-    '''
-            0 1 2 3 4 
-    nums = [3,4,5,1,2]
-                  l           
-                    r   
-            0 1 2 3 4 5 6        
-    nums = [4,5,6,7,0,1,2]
-                    l 
-                    m r
-            0  1  2  3   4
-    nums = [20,11,13,15,17]
-            l  m   
-                  r
-            0,1
-    nums = [2,1]
-            l r
-    '''
-    def findMin(self, nums: List[int]) -> int:
-        if len(nums) == 0: return nums[0]
-        l, r = 0, len(nums)-1
-        while l < r:
-            m = l + (r-l)//2
-            if nums[m] < nums[r]:
-                r = m
-            else:
-                l = m + 1
-        return nums[l]
+    def findMin(self, nums: List[int]) -> int:
+        '''
+        |     /  
+        |   /     /
+        | /     /  
+        |_____________
+          3 4 5 1 2
+               \U0001f447       \U0001f447  
+        nums = [3,4,5,1,2] ... nums[mid] > nums[hi] => min val is on the right 
+                    \U0001f446         
+               \U0001f447        \U0001f447
+        nums = [11,13,15,17] ... nums[mid] < nums[hi] => min val in on the left
+                       \U0001f446
+        '''
+        lo, hi = 0, len(nums)-1
+        while lo < hi:
+            mid = (lo + hi)//2 
+            if nums[mid] > nums[hi]:
+                lo = mid + 1
+            else:
+                hi = mid
+        return nums[lo]
