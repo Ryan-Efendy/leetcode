510352d08557dc5edea06cd6cf9c8b6bdbd0ebee
Ryan Efendy <ryanefendy95@gmail.com>
2021-04-27 10:35:06 -0700

    Time: 96 ms (72.16%), Space: 18.3 MB (76.38%) - LeetHub

--- kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.py
+++ kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.py
@@ -1,16 +1,26 @@
-import bisect
-from typing import List
-
 class KthLargest:
-    """
-    Binary Search + Insertion
-    Time: O(NlogN)
-    Space: O(N)
-    """
+    '''
+    https://www.youtube.com/watch?v=aaWU4Bq1X-c
+    https://leetcode.com/problems/kth-largest-element-in-a-stream/discuss/232147/Simplest-Python-MinHeap-Solution
+    
+    If you want to find Kth largest element maintain MinHeap of K elements similarlary if you want Kth smallest element maintain MaxHeap of k elements.
+    The element of the heap would be the Kth element.
+    '''
+
     def __init__(self, k: int, nums: List[int]):
+        self.heap = nums
+        heapq.heapify(self.heap)
         self.k = k
-        self.nums = sorted(nums)
 
     def add(self, val: int) -> int:
-        bisect.insort_left(self.nums, val)
-        return self.nums[len(self.nums) - self.k]
+        heapq.heappush(self.heap,val)
+        # if heap grows bigger then k remove elements
+        while len(self.heap) > self.k:
+            heapq.heappop(self.heap)
+        return self.heap[0]
+        
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
