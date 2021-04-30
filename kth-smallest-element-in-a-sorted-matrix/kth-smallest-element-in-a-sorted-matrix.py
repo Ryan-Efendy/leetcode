7ce82256fec83c64638cfa8a66dc715cb47c3a82
Ryan Efendy <ryanefendy95@gmail.com>
2021-04-30 16:08:41 -0700

    Time: 212 ms (36.47%), Space: 20 MB (79.25%) - LeetHub

--- kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.py
+++ kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.py
@@ -1,25 +1,25 @@
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
-        def isCountGreaterThanOrEqualToK(num: int) -> bool:
-            row, col = 0, len(matrix) - 1
-            count = 0
-            while row < len(matrix) and col >= 0:
-                if matrix[row][col] <= num:
-                    count += col + 1
-                    row += 1
-                else:
-                    col -= 1
-            return count >= k
+        '''
+        https://www.youtube.com/watch?v=cI3ivGemzsg
+        
+        matrix = [   0  1  2
+                 0 [ 1, 5, 9],    k = 8
+                 1 [10,11,13],
+                 2 [12,13,15]
+                 ]
+        
+        '''
+        minHeap = []
+        for i, num in enumerate(matrix[0]):
+            heappush(minHeap, (num, 0, i)) # num, row, col 
 
-        # find lower & upper bound of binarySearch
-        left, right = matrix[0][0], matrix[-1][-1]
+        while minHeap:
+            num, row, col = heappop(minHeap)
+            k -= 1
+            if k == 0:
+                return num
 
-        while left < right:
-            mid = left + (right - left) // 2
-
-            if isCountGreaterThanOrEqualToK(mid):
-                right = mid # b/c mid is valid need to be considered still
-            else:
-                left = mid + 1
-
-        return left
+            if row + 1 < len(matrix[i]):
+                heappush(minHeap, (matrix[row+1][col], row+1, col))
+        return -1
