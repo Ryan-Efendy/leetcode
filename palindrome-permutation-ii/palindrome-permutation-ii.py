7ee464eef0b7562e647229cfc4f6b98f938b4760
Ryan Efendy <ryanefendy95@gmail.com>
2021-05-02 13:40:06 -0700

    Time: 144 ms (70.34%), Space: 22.3 MB (28.51%) - LeetHub

--- palindrome-permutation-ii/palindrome-permutation-ii.py
+++ palindrome-permutation-ii/palindrome-permutation-ii.py
@@ -2,9 +2,28 @@ class Solution:
     def generatePalindromes(self, s: str) -> List[str]:
         '''
         1. get char freq, if odd > 1: return
-        2. bactracking
+        2. bactracking (generate only the first half of the palindromic str & append the reverse str of itself to generate the full palindromic str)
             a. add odd char first
             b. add even char to the beginning & end
+            
+        s = "aaalyal"
+        counter = { ✅ less than or equal to 1 odd char
+            'a': 4,
+            'l': 2,
+            'y': 1
+        }
+        
+        st = 'aal' # char freq reduced in half
+        
+         a                             a
+                                       |
+         a                             aa
+                                /      |          \  
+                              /        |           \
+         l                 laa         ala         aal            
+         
+    cur + mid + cur[::-1] laayaal   alayala       aalylaa
+                            ✅         ✅             ✅
         '''
         # s = "aabb"
         kv = collections.Counter(s) # {'a': 2, 'b': 2}
@@ -12,6 +31,7 @@ class Solution:
         if len(mid) > 1:
             return []
 
+        # add odd char first
         mid = '' if mid == [] else mid[0]
         half = ''.join([k * (v//2) for k, v in kv.items()]) # half: "ab"
         half = [c for c in half] # half: ["a","b"]
@@ -19,6 +39,7 @@ class Solution:
         def backtrack(end, tmp):
             if len(tmp) == end:
                 cur = ''.join(tmp) # cur: "ab"
+                # add even char to the beginning & end
                 ans.append(cur + mid + cur[::-1]) # ans: ["abba"]
             else:
                 for i in range(end):
