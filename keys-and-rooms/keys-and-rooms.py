d417fa97b52f00b2ce670743e2f55717bfe4b2a5
Ryan Efendy <ryanefendy95@gmail.com>
2021-05-03 22:32:08 -0700

    Time: 68 ms (41.14%), Space: 15 MB (19.64%) - LeetHub

--- keys-and-rooms/keys-and-rooms.py
+++ keys-and-rooms/keys-and-rooms.py
@@ -1,14 +1,34 @@
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
-        visited = [0] * len(rooms)
-        self.dfs(rooms, 0, visited)
-        return 0 not in visited
-    
-    def dfs(self, graph, node, visited):
-        if not visited[node]:
-            visited[node] = 1
-            for neighbour in graph[node]:
-                self.dfs(graph, neighbour, visited)
-                    
+        '''
+        https://www.youtube.com/watch?v=DoHwPeLKQmw
+        https://www.youtube.com/watch?v=8iUFLUIeK9k
         
-    
+                      0   1   2   3
+        Ex 1 Input: [[1],[2],[3],[]] => room0 -> room1 -> room2 -> room3
+             Output: true
+             We start in room 0, and pick up key 1.
+             We then go to room 1, and pick up key 2.
+             We then go to room 2, and pick up key 3.
+             We then go to room 3. Since we were able to go to every room, we return true. 
+        
+                                                             / \ <- cycle        /\ <- cycle  
+        Ex 2 Input: [[1,3],[3,0,1],[2],[0]] => room0 <-> room1 /            room2 / 
+                                                 ^         |
+                                                 |         V
+                                                 |-----> room3 
+             Output: false   
+             Explanation: We can't enter the room with number 2.
+             
+        from src vertex/node can we reach the rest of the nodes
+        '''
+        visited = set([0])
+        
+        def dfs(room):
+            for neib in rooms[room]:
+                if neib not in visited:
+                    visited.add(neib)
+                    dfs(neib)
+                    
+        dfs(0)
+        return len(visited) == len(rooms)    
