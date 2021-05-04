fff51fdd333593dd07d578c82a4796013af9a7a4
Ryan Efendy <ryanefendy95@gmail.com>
2021-05-04 11:49:24 -0700

    Time: 228 ms (98.38%), Space: 16.8 MB (77.60%) - LeetHub

--- walls-and-gates/walls-and-gates.py
+++ walls-and-gates/walls-and-gates.py
@@ -1,29 +1,23 @@
 class Solution:
     def wallsAndGates(self, rooms: List[List[int]]) -> None:
         """
-        Do not return anything, modify rooms in-place instead.
+        BFS enqueue all 0s, poll 0s check neighbor == INF then update it to curr+1
         """
         if not rooms or not len(rooms) or not len(rooms[0]): return
         m, n = len(rooms), len(rooms[0])
-        visitted = set()
+        INF = 2**31 - 1
         q = collections.deque()
-        # init
+        
+        # enqueue all 0s
         for i in range(m):
             for j in range(n):
-                if rooms[i][j] == 0: # check if it's a gate
+                if rooms[i][j] == 0:
                     q.append((i, j))
-                    visitted.add((i, j))
-        
-        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+                    
         while q:
             i, j = q.popleft()
-            for dx, dy in directions:
+            for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
                 x, y = i + dx, j + dy
-                if x >= 0 and x < m and y >= 0 and y < n and rooms[x][y] != -1 and (x, y) not in visitted:
-                    rooms[x][y] = min(rooms[x][y], rooms[i][j] + 1)
+                if x >= 0 and x < m and y >= 0 and y < n and rooms[x][y] == INF:
+                    rooms[x][y] = rooms[i][j] + 1
                     q.append((x, y))
-                    visitted.add((x, y))
-                    
-            
-            
-        
