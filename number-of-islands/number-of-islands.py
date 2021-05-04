817edf15446fdfa5ee6b6369ef1474e1dbe83763
Ryan Efendy <ryanefendy95@gmail.com>
2021-05-04 08:37:28 -0700

    Time: 148 ms (40.14%), Space: 19.6 MB (6.28%) - LeetHub

--- number-of-islands/number-of-islands.py
+++ number-of-islands/number-of-islands.py
@@ -1,26 +1,22 @@
 class Solution:    
     def numIslands(self, grid: List[List[str]]) -> int:        
         if not grid or not len(grid) or not len(grid[0]): return 0
-        
-        directions = [(1,0), (0,1), (-1,0), (0,-1)]
-        queue = collections.deque()
         visited = set()
-        islands = 0
+        res = 0
         m, n = len(grid), len(grid[0])
         
-        def bfs(grid, i, j):
-            while queue:
-                i, j = queue.popleft()
-                for direction in directions:
-                    x, y = i + direction[0], j + direction[1]
-                    if x >= 0 and x < m and y >= 0 and y < n and (x, y) not in visited and grid[x][y] == '1':
-                        queue.append((x, y))
-                        visited.add((x, y))
-
+        def dfs(i, j):
+            visited.add((i,j)) # grid[i][j] = '#'
+            # check up, right, down, left
+            for dx, dy in [(1,0), (0,1), (-1,0), (0,-1)]:
+                x = i + dx
+                y = j + dy
+                if 0 <= x < m and 0 <= y < n and (x,y) not in visited and grid[x][y] == '1':
+                    dfs(x, y)
+        
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == '1' and (i, j) not in visited:
-                    queue.append((i, j))
-                    bfs(grid, i, j)
-                    islands += 1
-        return islands
+                    res += 1
+                    dfs(i, j)
+        return res
