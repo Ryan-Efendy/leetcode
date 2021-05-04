d293decf186a2f326ab167718a03cdfe659e016a
Ryan Efendy <ryanefendy95@gmail.com>
2021-05-04 09:55:09 -0700

    Time: 76 ms (59.30%), Space: 14.7 MB (9.65%) - LeetHub

--- flood-fill/flood-fill.py
+++ flood-fill/flood-fill.py
@@ -1,14 +1,35 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
-        srcColor = image[sr][sc]
-        if srcColor == newColor: return image
-        image[sr][sc] = newColor
-        self.dfs(image, sr, sc, srcColor, newColor)
-        return image
+        '''
+        DFS: Find a connected component (CC) of (sc, sr)
+        Replace the color of all node in that CC
         
-    def dfs(self, image: List[List[int]], sr: int, sc: int, srcColor:int, newColor: int):
-        for r, c in [(1,0), (-1,0), (0,1), (0,-1)]:
-            i, j = sr + r, sc + c
-            if 0 <= i < len(image) and 0 <= j < len(image[0]) and image[i][j] == srcColor:
-                image[i][j] = newColor
-                self.dfs(image, i, j, srcColor, newColor)
+        [               sr = 1, sc = 1, newColor = 2
+            [1,1,1],
+            [1,1,0],    image[sr][sc] = 1
+            [1,0,1]     go up, down, left & right if CC color is the same as image[sr][sc] fill it
+        ]
+        
+        [
+            [2,2,2],
+            [2,2,0],
+            [2,0,1]
+        ]
+        '''
+        res = 0
+        m, n = len(image), len(image[0])
+        srcColor = image[sr][sc]
+
+        def dfs(i, j):
+            image[i][j] = newColor
+            for dx, dy in [(1,0), (0,1), (-1,0), (0,-1)]:
+                x = i + dx
+                y = j + dy
+                if 0 <= x < m and 0 <= y < n and image[x][y] == srcColor:
+                    dfs(x, y)
+
+        if image[sr][sc] == newColor:
+            return image
+        
+        dfs(sr, sc)
+        return image
